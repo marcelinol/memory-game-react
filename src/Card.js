67a17b20ecd77975d20ebc173c0569c4f299f75a
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2020-10-31 18:23:58 -0400

    Add  back of the memory card

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,8 @@
 const Card = ({ id }) => {
   const images = require.context('./images', true);
+  // <img src={images(`./${id}.jpg`).default} alt={id} className="card" />
   return (
-    <img src={images(`./${id}.jpg`).default} alt={id} className="card" />
+    <img src={images(`./cardBack.jpg`).default} alt="memory card" className="card" />
   );
 }
 
