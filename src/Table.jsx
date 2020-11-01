803ed2ac40e5261e976a192315eebe32949d67b4
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2020-11-01 18:36:14 -0500

    Add "you won" message
    and change table style

--- src/Table.jsx
+++ src/Table.jsx
@@ -6,6 +6,7 @@ function Table({ cards, images }) {
   const cardNeutralImage = images('./neutral.jpg').default;
 
   const [cardsRevealed, setCardsRevealed] = useState([]);
+  const [displayVictory, setDisplayVictory] = useState(false);
 
   const revealCard = (id, imageName) => {
     if (cardsRevealed.length === 2) {
@@ -39,63 +40,67 @@ function Table({ cards, images }) {
   useEffect(() => {
     const playableCards = cards.find((c) => c.imagePath !== cardNeutralImage);
     if (typeof playableCards === 'undefined') {
+      setDisplayVictory(true);
       console.log('YOU WON');
     }
   });
 
   return (
-    <table>
-      <tbody>
-        <tr>
-          {cards.slice(0, 4).map((card) => {
-            const { id, imageName, imagePath } = card;
-            return (
-              <td key={id}>
-                <Card
-                  id={id}
-                  onClick={revealCard}
-                  imagePath={imagePath}
-                  imageName={imageName}
-                  disabledState={cardNeutralImage}
-                />
-              </td>
-            );
-          })}
-        </tr>
-        <tr>
-          {cards.slice(4, 8).map((card) => {
-            const { id, imageName, imagePath } = card;
-            return (
-              <td key={id}>
-                <Card
-                  id={id}
-                  onClick={revealCard}
-                  imagePath={imagePath}
-                  imageName={imageName}
-                  disabledState={cardNeutralImage}
-                />
-              </td>
-            );
-          })}
-        </tr>
-        <tr>
-          {cards.slice(8, 12).map((card) => {
-            const { id, imageName, imagePath } = card;
-            return (
-              <td key={id}>
-                <Card
-                  id={id}
-                  onClick={revealCard}
-                  imagePath={imagePath}
-                  imageName={imageName}
-                  disabledState={cardNeutralImage}
-                />
-              </td>
-            );
-          })}
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      {displayVictory ? <h2>YOU WON!!!</h2> : null}
+      <table>
+        <tbody>
+          <tr>
+            {cards.slice(0, 4).map((card) => {
+              const { id, imageName, imagePath } = card;
+              return (
+                <td key={id}>
+                  <Card
+                    id={id}
+                    onClick={revealCard}
+                    imagePath={imagePath}
+                    imageName={imageName}
+                    disabledState={cardNeutralImage}
+                  />
+                </td>
+              );
+            })}
+          </tr>
+          <tr>
+            {cards.slice(4, 8).map((card) => {
+              const { id, imageName, imagePath } = card;
+              return (
+                <td key={id}>
+                  <Card
+                    id={id}
+                    onClick={revealCard}
+                    imagePath={imagePath}
+                    imageName={imageName}
+                    disabledState={cardNeutralImage}
+                  />
+                </td>
+              );
+            })}
+          </tr>
+          <tr>
+            {cards.slice(8, 12).map((card) => {
+              const { id, imageName, imagePath } = card;
+              return (
+                <td key={id}>
+                  <Card
+                    id={id}
+                    onClick={revealCard}
+                    imagePath={imagePath}
+                    imageName={imageName}
+                    disabledState={cardNeutralImage}
+                  />
+                </td>
+              );
+            })}
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
